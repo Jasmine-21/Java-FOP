0419288c56ef6cda1df2b104a4819d82c25fe526
Chinmay <chinmaykapoor27@gmail.com>
2019-11-07 09:51:08 +0530

    postorder

---
+++ PostOrder.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+
+class PostOrder 
+	{ 
+	    Node root; 
+	  
+	    PostOrder() 
+	    { 
+	        root = null; 
+	    } 
+	   
+	    void printPostorder(Node node) 
+	    { 
+	        if (node == null) 
+	            return; 
+	        printPostorder(node.left);
+	        printPostorder(node.right); 
+	  
+	        System.out.print(node.key + " "); 
+	    } 
+	    
+	    void printPostorder()    {     printPostorder(root);   } 
+	  
+	    // Driver method 
+	    public static void main(String[] args) 
+	    { 
+	        PostOrder tree = new PostOrder(); 
+	        tree.root = new Node(1); 
+	        tree.root.left = new Node(2); 
+	        tree.root.right = new Node(3); 
+	        tree.root.left.left = new Node(4); 
+	        tree.root.left.right = new Node(5); 
+	  
+	        
+	  
+	        System.out.println("\nPostorder traversal of binary tree is "); 
+	        tree.printPostorder(); 
+	  
+	        
+	    }
+	}
+
+
