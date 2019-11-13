6ff4ff8681d0e2fc50f2b7adc3fe847259e19f60
Jasmine-21 <55360377+Jasmine-21@users.noreply.github.com>
2019-11-13 18:32:18 +0530

    Queue

---
+++ head5th.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class head5th {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Queue<Integer> queue=new LinkedList<Integer>();
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+while(n-->0)
+{
+	queue.add(s.nextInt());
+}
+printFifthElementFromStart(queue);
+
+	}
+    static void printFifthElementFromStart(Queue<Integer> queue) {
+    int n=queue.size();
+    if(n<5)
+    {
+        System.out.println("There are no enough elements in the queue");
+
+    }
+    else
+    {
+    	for (int i = 0; i < 4; i++)
+            queue.remove();
+        System.out.println(queue.peek());
+    }
+    }
+
+}
