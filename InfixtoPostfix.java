632a38e4177a79d2c2f50633880a1a5bc6aaef00
Jasmine-21 <55360377+Jasmine-21@users.noreply.github.com>
2019-11-17 11:24:06 +0530

    queue

---
+++ InfixtoPostfix.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+class InfixtoPostfix{
+    public static int getprec(char element) {
+        if (element == '+' || element == '-') {
+            return 1;
+        } else if (element == '*' || element == '/') {
+            return 2;
+        } else if (element == '^') {
+            return 3;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String arg[]) {
+        Stack<Character> s = new Stack<Character>();
+        Scanner in = new Scanner(System.in);
+
+        String exp = in.nextLine(), answer = "";
+
+        for (char ch : exp.toCharArray()) {
+            if (Character.isLetterOrDigit(ch)) {
+                answer += ch;
+
+            } else if (ch == '(') {
+                s.push('(');
+            } else if (ch == ')') {
+                while (s.peek() != '(' && !s.empty()) {
+                    answer += s.pop();
+                }
+                if (s.empty()) {
+                    System.out.println("Invalid Expression");
+                } else {
+                    //pop '('
+                    s.pop();
+                }
+            } else {
+                while (!s.empty() && getprec(ch) <= getprec(s.peek())) {
+                    answer += s.pop();
+                }
+                s.push(ch);
+            }
+
+        }
+        while (!s.empty()) {
+            answer += s.pop();
+        }
+        System.out.println(answer);
+
+    }
+}
