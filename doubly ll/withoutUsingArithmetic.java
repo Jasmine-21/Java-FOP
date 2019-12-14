041c16ae60f5620a4e1e7bd7a77690876fc6d1b4
Jasmine-21 <55360377+Jasmine-21@users.noreply.github.com>
2019-12-14 12:40:56 +0530

    add two numbers

--- doubly ll/withoutUsingArithmetic.java
+++ doubly ll/withoutUsingArithmetic.java
@@ -1,21 +1,34 @@
-//package github;
+package github;
 
 import java.util.*;
 
 public class withoutUsingArithmetic {
 
 	public static void main(String[] args) {
-		int a, b ;
-		   Scanner sc = new Scanner(System.in);	
-		   System.out.println("Enter a: ");
-		   a = sc.nextInt(); 
-		   System.out.println("Enter b: ");
-		   b = sc.nextInt(); 
-	      while(b != 0){
-	            int c = a & b;
-	            a = a ^ b;
-	            b = c << 1;
+		// TODO Auto-generated method stub
+//		int a, b ;
+//		   Scanner in = new Scanner(System.in);	
+//		   System.out.print("Enter a: ");
+//		   a = in.nextInt(); 
+//		   System.out.print("Enter b: ");
+//		   b = in.nextInt(); 
+//	      while(b != 0){
+//	           int  c = a & b;
+//	           // System.out.print(c);
+//	            a = a ^ b;
+//	            b = c << 1;
+//	        }
+//	        System.out.print("Sum: "+a);
+//	        System.out.print("Sum: "+b);
+		 Scanner sc=new Scanner(System.in);
+	        int num1=sc.nextInt();
+	        int num2=sc.nextInt();
+	        while(num1!=0)
+	        {
+	            num2++;
+	            num1--;
 	        }
-	        System.out.println("Sum: "+a); 
+	        System.out.println("result="+num2);
+	        
 	}
 }
