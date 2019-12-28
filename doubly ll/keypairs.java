1b9944452db06f58493775e42486846a1c9f500e
Jasmine-21 <55360377+Jasmine-21@users.noreply.github.com>
2019-12-28 20:30:31 +0530

    Add files via upload

---
+++ doubly ll/keypairs.java
@@ -0,0 +1,43 @@
+package github;
+
+import java.util.*; 
+
+class Keypairs {     
+	static boolean hasArrayTwoCandidates(int A[], int arr_size, int sum) 
+    { 
+        int l, r; 
+        Arrays.sort(A);
+        l = 0; 
+        r = arr_size - 1; 
+        while (l < r) 
+        { 
+            if (A[l] + A[r] == sum) 
+                return true;
+            else if (A[l] + A[r] < sum) 
+                l++; 
+            else 
+                r--; 
+        } 
+                return false;
+    } 
+  
+    public static void main(String args[]) 
+    { 
+       Scanner sc=new Scanner(System.in);
+       int t=sc.nextInt();
+       int n=sc.nextInt();
+
+       int A[]=new int[t];
+       for(int i=0;i<t;i++)
+       {
+    	   A[i]=sc.nextInt();
+       }
+        int arr_size = A.length;
+        // hasArrayTwoCandidates(A, arr_size, n);
+        if(hasArrayTwoCandidates(A, arr_size, n)==true)
+        System.out.println("Yes");
+        else
+        System.out.println("No");
+           
+    } 
+}
